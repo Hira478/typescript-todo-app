e1cc44d56fb74b449b92f4383108af4ad27de02d
hirana <daush128@gmail.com>
2024-06-17 14:39:06 +0700

    first commit

---
+++ src/components/Todos.tsx
@@ -0,0 +1,32 @@
+import TodoItem from "./TodoItem";
+import { Todo } from "../TodoType";
+
+type TodosProps = {
+  todos: Todo[];
+  toggleCompleted: (todoId: number) => void;
+};
+
+const Todos = ({ todos, toggleCompleted }: TodosProps): JSX.Element => {
+  return (
+    <div style={styles.container}>
+      {todos.map((todo) => {
+        return (
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            toggleCompleted={toggleCompleted}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+const styles = {
+  container: {
+    width: "40%",
+    margin: "0 auto",
+  },
+};
+
+export default Todos;
